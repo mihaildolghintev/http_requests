597614ef8eeb9f1ffc3acb4fc878a5e5d71ce669
Mike Dolghintev <yadolghintev@gmail.com>
2021-10-15 19:49:29 +0300

    added env

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
+gem 'dotenv'
 gem 'rack-test'
 gem 'rest-client'
 gem 'rexml'
--- config.rb
+++ config.rb
@@ -1,11 +1,12 @@
+require 'dotenv/load'
 require_relative 'spotify/spotify'
 
 Spotify.config do |config|
-  config.client_id = '242f1510a4244b92853e89bbdff00560'
-  config.client_secret = '8641bb5a1da043c6ac86eb5dec86132b'
+  config.client_id = ENV['CLIENT_ID']
+  config.client_secret = ENV['CLIENT_SECRET']
   config.scope = 'playlist-modify-public'
   config.redirect_uri = 'http://example.com'
-  config.email = 'yadolghintev@gmail.com'
-  config.password = 'Dolghintev1'
+  config.email = ENV['EMAIL']
+  config.password = ENV['PASSWORD']
   config.get_token_base_url = 'https://accounts.spotify.com/api/token'
 end
